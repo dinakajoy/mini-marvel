353b31964901abc4b1ac16c0a8d32053b73c4a51
odinaka-joy <odinakajoy909@gmail.com>
2020-07-02 01:00:54 +0100

    setup dev environment

--- src/components/common/Header.js
+++ src/components/common/Header.js
@@ -1,9 +1,13 @@
 import React from 'react';
+import largeLogo from '../../images/logo-lg.png';
+// import smallLogo from '../../images/logo-sm.png';
 import './Common.css';
 
 function Header() {
   return (
-    <header className="App-header"></header>
+    <header className="App-header">
+      <img src={largeLogo} alt="Marvel" />
+    </header>
   );
 }
 
